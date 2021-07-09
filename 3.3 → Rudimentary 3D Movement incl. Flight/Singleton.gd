2eb1b90c7a6c31966a2ff6b7193596ad5d521995
Yuminous <79890259+Yuminous@users.noreply.github.com>
2021-07-09 19:27:12 +1200

    Update Singleton.gd

--- 3.3 → Rudimentary 3D Movement incl. Flight/Singleton.gd
+++ 3.3 → Rudimentary 3D Movement incl. Flight/Singleton.gd
@@ -3,6 +3,7 @@ extends Spatial
 onready var menu = $Label
 onready var player = $KinematicBody
 onready var player_head = $KinematicBody/Spatial
+onready var player_body = $KinematicBody/MeshInstance
 onready var camera = $KinematicBody/Spatial/Camera
 
 var gravity = true
@@ -10,9 +11,8 @@ var pause = true
 var tps = false
 
 var vertical_movement = 0
-var sprint_multiplier = 1
-var freefall_multiplier = 1
-
+var speed_multiplier = 1
+var terminal_velocity = -100
 
 
 func menu():
@@ -26,77 +26,72 @@ func menu():
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
-
 func _physics_process(delta):
 	if pause == true:
 		return
 		
 	if gravity == true:
-		player.move_and_slide(Vector3(0, vertical_movement, 0), Vector3.UP)
-	if player.is_on_floor() == true:
-		freefall_multiplier = 1
+		player.move_and_slide(Vector3(0, max(vertical_movement, terminal_velocity), 0), Vector3.UP)
+	if player.is_on_floor() == true or player.is_on_wall() == true:
+		vertical_movement = 0
+		terminal_velocity = -100
 		if Input.is_action_pressed("sprint"):
-			sprint_multiplier = 2
+			speed_multiplier = 2
 		if Input.is_action_just_pressed("jump"):
-			vertical_movement = 7	
+			vertical_movement = 10
+		if Input.is_action_just_pressed("walk"):
+			speed_multiplier = 0.5
 	else:
-		vertical_movement -= 10 * delta * freefall_multiplier
-		freefall_multiplier += delta * 3
-		
+		vertical_movement -= 20 * delta 
+		if Input.is_action_pressed("sprint"):
+			terminal_velocity = -2
+			vertical_movement = -2
 	if Input.is_action_pressed("forward"):
-		player.move_and_slide(Vector3(0, 0, -11 * sprint_multiplier).rotated(Vector3.UP, player.rotation.y), Vector3.UP, true)
+		player.move_and_slide(Vector3(0, 0, -11 * speed_multiplier).rotated(Vector3.UP, player.rotation.y), Vector3.UP, true)
 	if Input.is_action_pressed("back"):
-		player.move_and_slide(Vector3(0, 0, 11 * sprint_multiplier).rotated(Vector3.UP, player.rotation.y), Vector3.UP, true)
+		player.move_and_slide(Vector3(0, 0, 11 * speed_multiplier).rotated(Vector3.UP, player.rotation.y), Vector3.UP, true)
 	if Input.is_action_pressed("left"):
-		player.move_and_slide(Vector3(-11 * sprint_multiplier, 0, 0).rotated(Vector3.UP, player.rotation.y), Vector3.UP, true)
+		player.move_and_slide(Vector3(-11 * speed_multiplier, 0, 0).rotated(Vector3.UP, player.rotation.y), Vector3.UP, true)
 	if Input.is_action_pressed("right"):
-		player.move_and_slide(Vector3(11 * sprint_multiplier, 0, 0).rotated(Vector3.UP, player.rotation.y), Vector3.UP, true)
-	if Input.is_action_pressed("sprint"):
-		freefall_multiplier = 0.2
-	elif Input.is_action_just_released("sprint"):
-		sprint_multiplier = 1
-		freefall_multiplier = 1
-		
-	
-	# the method of flight here below is very crude, based simply on the camera rotation, but it can be tuned better so that players don't notice or don't mind.
-	# o método de voo aqui é muito básico, é simplesmente baseado na rotação da câmara, mas pode ser melhorado de modo a que os jogadores não se apercebam da diferença.
-	
+		player.move_and_slide(Vector3(11 * speed_multiplier, 0, 0).rotated(Vector3.UP, player.rotation.y), Vector3.UP, true)
+	if Input.is_action_just_released("sprint"):
+		speed_multiplier = 1
+		terminal_velocity = -100
+	if Input.is_action_just_released("walk"):
+		speed_multiplier = 1
 	if Input.is_action_pressed("fly"):
 		gravity = false
-		sprint_multiplier = 1
-		freefall_multiplier = 1
+		speed_multiplier = 1
 		vertical_movement = 0
-		var A = player_head.rotation_degrees.x / 3  # <<< flight up or down is based on camera rotation, finely tune this by changing the offset "/ 3"
+		var A = player_head.rotation_degrees.x / 3 
 		if Input.is_action_pressed("sprint"):
-			sprint_multiplier = 2
-			A = A * 1.5  # <<< climb rate is multiplied here while "sprinting" in the air to maintain flight attitude, again, modify the offset if need be
-		
-		
+			speed_multiplier = 2
+			A = A * 1.5
 		if Input.is_action_pressed("forward"):
 			player.move_and_slide(Vector3(0, A, 0), Vector3.UP)
 	elif Input.is_action_just_released("fly"):
 		gravity = true
-		sprint_multiplier = 1
+		speed_multiplier = 1
 	if Input.is_action_just_pressed("camera"):
 		if tps == true:
+			player_body.hide()
 			camera.set_translation(Vector3(0, 0.2, -0.4))
 			camera.rotation_degrees.x = -0
 			tps = false
 		elif tps == false:
+			player_body.show()
 			camera.set_translation(Vector3(0, 0.2, 4.4))
 			camera.rotation_degrees.x = -10
 			tps = true		
 
 
-
 func _unhandled_input(event):
 	if event is InputEventMouseMotion and pause == false:
 		player.rotate_y(-event.relative.x/150)
-		player_head.rotation_degrees.x = clamp(player_head.rotation_degrees.x - event.relative.y/5, -50, 90)
+		player_head.rotation_degrees.x = clamp(player_head.rotation_degrees.x - event.relative.y/5, -70, 95)
 		
 	if event.is_action_pressed("pause"):
 		menu()
-
 
 
 func _on_Button_pressed():
